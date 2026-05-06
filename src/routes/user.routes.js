05cf36262f2157918da610c664cabfcd4452ee22
ndn-elias <elias.nodon@iut-tlse3.fr>
2026-05-06 14:07:12 +0200

    ajout middleware auth dans update user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { findAll, findById, login, loginAdmin, register, updateUserAdmin } from "../controllers/user.controller.js";
 import { registerValidator, loginValidator } from "../validators/user.validator.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authMiddleware, isAdmin } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -12,6 +12,6 @@ router.post("/register", registerValidator, validate, register);
 router.post("/login", loginValidator, validate, login);
 router.post("/login-admin", loginValidator, validate, loginAdmin);
 router.put("/update", registerValidator, validate, register);
-router.patch("/:id", authMiddleware, updateUserAdmin);
+router.patch("/:id", authMiddleware, isAdmin, updateUserAdmin);
 
 export default router;
